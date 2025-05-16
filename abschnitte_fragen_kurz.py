ce16d5554a8ee3a05ef15c2fec2274cbf004f964
Daniel-apper <daniel@therapie-vonwil.de>
2025-05-16 02:15:55 +0200

    Add files via upload

---
+++ abschnitte_fragen_kurz.py
@@ -0,0 +1,23 @@
+
+abschnitte = {
+    "Wahrnehmung & soziale Kommunikation": [
+        "Ich finde es anstrengend, herauszufinden, wann ich in einem Gespräch an der Reihe bin.",
+        "Ich finde es leichter, allein zu sein als unter Menschen."
+    ],
+    "Emotionsregulation & Beziehungserleben": [
+        "Ich fühle mich oft für die Gefühle anderer verantwortlich.",
+        "Ich habe Angst, durch Grenzsetzung andere zu verletzen."
+    ],
+    "Maskierung und Selbstbild": [
+        "Ich beobachte andere, um zu wissen, wie ich mich verhalten soll.",
+        "Ich verstelle meine Körpersprache, um nicht negativ aufzufallen."
+    ],
+    "Sensorische Besonderheiten": [
+        "Ich erkenne oft Muster oder Strukturen in Kunst, Sprache, Verhalten oder Geräuschen.",
+        "Ich bin besonders aufmerksam für Details, die andere übersehen."
+    ],
+    "Trauma- und Bindungserleben": [
+        "Ich habe erlebt, dass ich meine Grenzen übergehen musste, um eine Beziehung zu erhalten.",
+        "Ich wurde emotional unter Druck gesetzt (z. B. durch Schuldgefühle oder Drohungen)."
+    ]
+}
